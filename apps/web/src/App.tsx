562cf618efb6c3e4c0a47c024dced0b30be7ff9e
Robert Rojas Fernández <robert.rojas@wizeline.com>
2024-10-07 14:38:26 -0500

    (feat) add QueryClientProvider

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,24 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProvider } from './context/AppContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Layout } from './components/Layouts/Default/Layout';
 import { HomePage } from './pages/HomePage';
 import LaunchesPage from './pages/LaunchesPage';
+
+// Create a client for react-query
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
-        <AppProvider>
-          <Router>
-            <Layout>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/launches" element={<LaunchesPage />} />
-              </Routes>
-            </Layout>
-          </Router>
-        </AppProvider>
+        <QueryClientProvider client={queryClient}>
+          <AppProvider>
+            <Router>
+              <Layout>
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/launches" element={<LaunchesPage />} />
+                </Routes>
+              </Layout>
+            </Router>
+          </AppProvider>
+        </QueryClientProvider>
       </ChakraProvider>
     </div>
   );
