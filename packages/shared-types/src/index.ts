f6fc0fc70af1001b00c419d6100c18fd1b381917
Robert Rojas Fernández <robert.rojas@wizeline.com>
2024-10-06 23:52:51 -0500

    (feat) create packages/shared-types

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,24 @@
+export interface Launch {
+  id: string;
+  name: string;
+  date_utc: string;
+  success: boolean | null;
+  details: string | null;
+  links: {
+    patch: {
+      small: string | null;
+      large: string | null;
+    };
+    webcast: string | null;
+  };
+  launchpad: string;
+}
+
+export type LaunchType = 'upcoming' | 'past' | 'all';
+
+export interface Tweet {
+  id: string;
+  content: string;
+  created_at: string;
+  author: string;
+}
