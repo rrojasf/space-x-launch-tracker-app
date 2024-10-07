e378a1bd4b3528159a013396db672b967194a89a
Robert Rojas Fernández <robert.rojas@wizeline.com>
2024-10-06 23:03:28 -0500

    (feat) implement basic home page

---
+++ apps/web/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, VStack, Heading } from '@chakra-ui/react';
+
+export const HomePage: React.FC = () => {
+  return (
+    <Box margin="0 auto" padding={4}>
+      <VStack spacing={8}>
+        <Heading as="h1" textAlign="center">
+          Space Launch Tracker
+        </Heading>
+      </VStack>
+    </Box>
+  );
+};
