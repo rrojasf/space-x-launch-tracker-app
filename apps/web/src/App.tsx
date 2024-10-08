775533a1c3549326983d825486eec9cb30066065
Robert Rojas Fernández <robert.rojas@wizeline.com>
2024-10-08 15:39:35 -0500

    (refactor) Add Suspense, ErrorBoundary and Lazy loading to App

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Suspense, lazy } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ErrorBoundary from "./components/common/ErrorBoundary";
 import { Layout } from "./components/Layouts/Default/Layout";
 import { AppProvider } from "./context/AppContext";
-import FavoritesPage from "./pages/FavoritesPage";
-import { HomePage } from "./pages/HomePage";
-import LaunchesPage from "./pages/LaunchesPage";
 
 // Create a client for react-query
 const queryClient = new QueryClient();
+
+const Home = lazy(() => import("./pages/HomePage"));
+const Launches = lazy(() => import("./pages/LaunchesPage"));
+const Favorites = lazy(() => import("./pages/FavoritesPage"));
+
+// todo: ¿refactor creating a Providers wrapper?
 
 function App() {
   return (
@@ -18,11 +23,15 @@ function App() {
           <AppProvider>
             <Router>
               <Layout>
-                <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/launches" element={<LaunchesPage />} />
-                  <Route path="/favorites" element={<FavoritesPage />} />
-                </Routes>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <ErrorBoundary>
+                    <Routes>
+                      <Route path="/" Component={Home} />
+                      <Route path="/launches" Component={Launches} />
+                      <Route path="/favorites" Component={Favorites} />
+                    </Routes>
+                  </ErrorBoundary>
+                </Suspense>
               </Layout>
             </Router>
           </AppProvider>
---
+++ apps/web/src/components/common/ErrorBoundary.tsx
@@ -0,0 +1,67 @@
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import React, { ErrorInfo } from "react";
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error?: Error;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <Box textAlign="center" py={10} px={6}>
+          <Heading
+            display="inline-block"
+            as="h2"
+            size="2xl"
+            bgGradient="linear(to-r, teal.400, teal.600)"
+            backgroundClip="text"
+          >
+            Oops!
+          </Heading>
+          <Text fontSize="18px" mt={3} mb={2}>
+            {this.state.error?.message || "An unexpected error occurred"}
+          </Text>
+          <Text color={"gray.500"} mb={6}>
+            We're sorry for the inconvenience. Please try again later.
+          </Text>
+
+          <Button
+            colorScheme="teal"
+            bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+            color="white"
+            variant="solid"
+            onClick={() => window.location.reload()}
+          >
+            Refresh Page
+          </Button>
+        </Box>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
