d6be0d52093c453ce453b7a3e87b993ede910756
Robert Rojas Fernández <robert.rojas@wizeline.com>
2024-10-08 17:09:49 -0500

    (refactor) remove CORS  bypass

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,12 +1,12 @@
-import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-import dotenv from "dotenv";
+import { errorHandler } from "./middleware/errorHandler";
 import healthRouter from "./routes/health";
 import launchRoutes from "./routes/launchRoutes";
 import twitterRoutes from "./routes/twitterRoutes";
-import { errorHandler } from "./middleware/errorHandler";
 
 dotenv.config();
 
@@ -17,20 +17,10 @@ app.use(helmet());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
-  }),
+  })
 );
 app.use(morgan("dev"));
 app.use(express.json());
-
-// Disable CORS while development stage
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
-  next();
-});
 
 // Routes
 app.use("/health", healthRouter);
