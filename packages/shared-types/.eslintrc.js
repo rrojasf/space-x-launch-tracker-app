e8fd738f1a8f8adf0f53330c8e9d788d7b0b6337
Robert Rojas Fernández <robert.rojas@wizeline.com>
2024-10-08 18:30:56 -0500

    (lint) add lint to shared-types

---
+++ packages/shared-types/.eslintrc.js
@@ -0,0 +1,12 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  extends: ["@space-launch-tracking-app/eslint-config"],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+  },
+  plugins: ["react"],
+  ignorePatterns: ["dist/**/*.js", "dist/**/*.d.ts", ".eslintrc.js"],
+};
+
+module.exports = config;
