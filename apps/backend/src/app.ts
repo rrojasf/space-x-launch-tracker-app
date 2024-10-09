49931fc2987961670060e44f80cbd4f9bb1a8edc
Robert Rojas Fernández <robert.rojas@wizeline.com>
2024-10-09 00:48:15 -0500

    (backend) add list of allowed origins

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -10,25 +10,36 @@ import twitterRoutes from "./routes/twitterRoutes";
 
 dotenv.config();
 
-const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:3000"];
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_URL2,
+  "http://localhost:3000",
+];
+
+// CORS options
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error(
+          "The CORS policy for this site does not allow access from the specified Origin."
+        )
+      );
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
 const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  }),
-);
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 
